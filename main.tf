2416cc46f1a4c1a9b98ad021a993a3a4e373b4b3
itschrisyo <91677096+itschrisyo@users.noreply.github.com>
2022-02-08 09:33:43 -0500

    encryptionhidden

--- main.tf
+++ main.tf
@@ -20,6 +20,14 @@ resource "aws_s3_bucket" "my-github-actions-bucket1979" {
   bucket =  "my-github-actions-bucket1979"
   acl = "public-read"
 
+  #server_side_encryption_configuration {
+  #  rule {
+  #    apply_server_side_encryption_by_default {
+  #      sse_algorithm = "AES256"
+  #    }
+  #  }
+  #}
+
   tags = {
     Name = "GitHubActions"
     Environment = "Dev"
