9554f4fdaccd510e7fc65efb0a58173ad81c98f1
Accurics <admin@accurics.com>
2022-02-08 10:14:37 -0500

    Auto Remediation By Accurics

--- main.tf
+++ main.tf
@@ -17,8 +17,8 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "my-github-actions-bucket1979" {
-  bucket =  "my-github-actions-bucket1979"
-  acl = "public-read"
+  bucket = "my-github-actions-bucket1979"
+  acl    = "public-read"
 
   #server_side_encryption_configuration {
   #  rule {
@@ -29,10 +29,14 @@ resource "aws_s3_bucket" "my-github-actions-bucket1979" {
   #}
 
   tags = {
-    Name = "GitHubActions"
+    Name        = "GitHubActions"
     Environment = "Dev"
   }
-  
+
+
+  versioning {
+    enabled = true
+  }
 }
 resource "aws_instance" "app_server" {
   count         = 3 # Will create 3 EC2 instances
@@ -42,7 +46,7 @@ resource "aws_instance" "app_server" {
   tags = {
     Tenable     = "FA"
     Environment = "Dev"
-    Name = "Compute"
+    Name        = "Compute"
   }
 }
 
@@ -51,31 +55,31 @@ resource "aws_security_group" "allow_ssh_dev_access3" {
   description = "Allow SSH access by Development Team"
   vpc_id      = "vpc-5bcd7721"
 
-  ingress = [ {
-    cidr_blocks = [ "0.0.0.0/0" ]
-    description = "Allow SSH Access from Dev"
-    from_port = 22
+  ingress = [{
+    cidr_blocks      = ["0.0.0.0/0"]
+    description      = "Allow SSH Access from Dev"
+    from_port        = 22
     ipv6_cidr_blocks = []
-    prefix_list_ids = []
-    protocol = "tcp"
-    security_groups = []
-    self = false
-    to_port = 22
-  } ]
+    prefix_list_ids  = []
+    protocol         = "tcp"
+    security_groups  = []
+    self             = false
+    to_port          = 22
+  }]
 
-  egress = [ {
-    cidr_blocks = [ "0.0.0.0/0" ]
-    description = "Allow All Outbound"
-    from_port = 0
+  egress = [{
+    cidr_blocks      = ["0.0.0.0/0"]
+    description      = "Allow All Outbound"
+    from_port        = 0
     ipv6_cidr_blocks = ["::/0"]
-    prefix_list_ids = []
-    protocol = "-1"
-    security_groups = []
-    self = false
-    to_port = 0
-  } ]
-  
+    prefix_list_ids  = []
+    protocol         = "-1"
+    security_groups  = []
+    self             = false
+    to_port          = 0
+  }]
+
   tags = {
     Name = "allow_ssh"
   }
-  }
+}
